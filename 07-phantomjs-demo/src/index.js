765db7c5f09e68f2a7c1b32d6b5c1391e7723454
theseawolves <theseawolves@yeah.net>
2016-07-27 05:44:27 +0800

    add phantomjs test

---
+++ 07-phantomjs-demo/src/index.js
@@ -0,0 +1,6 @@
+window.render = function(){
+  var ctn = document.createElement('div')
+  ctn.setAttribute('id','tmall')
+  ctn.appendChild(document.createTextNode('天猫前端招人'))
+  document.body.appendChild(ctn)
+}
